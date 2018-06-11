a093de718aea21bea45b9940480a470816e476ff
felixbepunkt <felix.batzke@gmail.com>
2018-06-12 01:12:15 +0200

    this is a CD/CI test. implemented test-mock, typo, removed not usable script command.

--- test/test.js
+++ test/test.js
@@ -1,9 +1,20 @@
 var expect  = require('chai').expect;
 var request = require('request');
 
+var assert = require('assert');
+describe('Array', function() {
+  describe('#indexOf()', function() {
+    it('should return -1 when the value is not present', function() {
+      assert.equal([1,2,3].indexOf(4), -1);
+    });
+  });
+});
+
+/*
 it('Main page content', function(done) {
     request('http://localhost:3000' , function(error, response, body) {
         expect(response.statusCode).to.equal(200);
         done();
     });
 });
+*/
